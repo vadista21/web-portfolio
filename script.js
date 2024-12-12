6db64dc46700b05cba507b918dc4bf75799aee87
Vadista Cahya Maharani <Vadistacahyamaharani@gmail.com>
2024-12-12 20:19:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const ctx = document.getElementById('salesChart').getContext('2d');
+
+const data = {
+    labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
+    datasets: [
+        {
+            label: '2022',
+            data: [4017, 6135, 7091, 5841, 5036, 4547, 3467, 3970, 6313, 3595, 9207, 5945],
+            backgroundColor: 'e4566c(54, 162, 235, 0.5)',
+            borderColor: 'e4566c(54, 162, 235, 1)',
+            borderWidth: 1
+        },
+        {
+            label: '2023',
+            data: [2416, 4136, 7935, 8004, 9505, 5026, 6108, 6343, 9404, 9280, 9287, 8689],
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        }
+    ]
+};
+
+const config = {
+    type: 'bar',
+    data: data,
+    options: {
+        responsive: true,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+};
+
+new Chart(ctx, config);
